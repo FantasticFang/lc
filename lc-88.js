ec493b171d62a35be47016a699baba9c5ac22469
weijie9 <weijie9@staff.weibo.com>
2024-06-24 22:52:56 +0800

    lc-88

---
+++ lc-88.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  for(let i = m, j = n; i > 0 || j > 0;) {
+    if(j === 0 || (i > 0 && nums1[i - 1] > nums2[j - 1])) {
+      nums1[i + j -1] = nums1[i - 1]
+      i--
+    } else {
+      nums1[i + j -1] = nums2[j - 1]
+      j--
+    }
+  }
+};
+
+merge([1,2,3,0,0], 3, [1,2], 2)
+
+// 
+// 终止条件：双指针走完
