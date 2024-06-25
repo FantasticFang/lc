cad3942040575167ea7cca5676f91c819b44462b
weijie9 <weijie9@staff.weibo.com>
2024-06-25 20:33:09 +0800

    update: 快慢指针

--- lc-27.js
+++ lc-27.js
@@ -3,6 +3,7 @@
  * @param {number} val
  * @return {number}
  */
+// 双指针法-首尾指针
 var removeElement = function (nums, val) {
   let m = nums.length
   for (let i = 0; i < m; ) {
@@ -15,3 +16,16 @@ var removeElement = function (nums, val) {
   }
   return m
 };
+
+// 双指针法-快慢指针，其中i一定走的快于k
+var removeElement = function (nums, val) {
+  let k = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i]
+      k++
+    }
+  }
+  return k
+};
+
