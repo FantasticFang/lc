bf6c6ab8e180edc3abf7c284205f75dbc8db129b
weijie9 <weijie9@staff.weibo.com>
2024-07-01 23:47:21 +0800

    lc-189

--- lc-189.js
+++ lc-189.js
@@ -11,6 +11,7 @@
 // o(n2)的时间复杂度，运行超时
 var rotate = function(nums, k) {
   const length = nums.length
+  k = k % length
   for(let i = 0; i < k; i++) {
     const last = nums[length - 1]
     for(let j = length - 1; j > 0; j--) {
@@ -19,3 +20,27 @@ var rotate = function(nums, k) {
     nums[0] = last
   }
 };
+
+// o(n)时间复杂度、o(n)空间复杂度
+var rotate = function(nums, k) {
+  const length = nums.length
+  // k可能是一个很大的值
+  k = k % length
+  const tmp = []
+  // copy
+  for(let i = 0; i < k; i++) {
+    tmp[i] = nums[length - k + i]
+  }
+  // 更换位置
+  for(let i = 0; i < length - k; i++) {
+    nums[length - 1 - i] = nums[length - 1 - i - k]
+  }
+  // 赋值 
+  for(let i = 0; i < k; i++) {
+    nums[i] = tmp[i]
+  }
+}
+
+var rotate = function(nums, k) { 
+
+}
