caf10d672c6c72f83b6a7dd360c174b5a998e40c
weijie9 <weijie9@staff.weibo.com>
2024-07-09 23:42:34 +0800

    lc-122

--- lc-121.js
+++ lc-121.js
@@ -21,5 +21,53 @@ var maxProfit = function(prices) {
   return max
 };
 
-console.log(maxProfit([7,1,5,3,6,4]))
+// 循环找出最低点，最高点
+var maxProfit = function(prices) {
+  let minIndex = 0, maxIndex = 0
+  let minValue = Number.MAX_SAFE_INTEGER, maxValue = 0
+  let max = 0
+  for(let i = 0; i < prices.length; i++) {
+    if(prices[i] > maxValue) {
+      maxValue = prices[i]
+      maxIndex = i
+    }
+    if(prices[i] < minValue) {
+      minValue = prices[i]
+      minIndex = i
+    }
 
+    if(maxIndex > minIndex) {
+      max = Math.max(max, maxValue - minValue)
+    } else {
+      maxValue = 0
+      maxIndex = 0
+    }
+  }
+  return max
+};
+
+// 递归思想：index=n的最大值 = max(n减去前n-1的最小值，n-1的最大利润)
+var maxProfit = function(prices){
+  let minValue = Number.MAX_SAFE_INTEGER 
+  function getMax(n) {
+    let max = 0
+    if(n > 0) {
+      max = Math.max(getMax(n - 1), prices[n] - minValue) // getMax函数需要在前面，以便在出栈时，再计算price[n] - minValue 
+    }
+    
+    if(minValue > prices[n]) {
+      minValue = prices[n]
+      
+    }
+    return max
+  }
+  return getMax(prices.length - 1)
+};
+
+console.log(maxProfit([1, 2, 3, 1, 7]))
+// stack
+// getMax(0)         minValue = prices[0], max = 0
+// ...                 
+// getMax(n - 3)  ->  
+// getMax(n - 2)
+// getMax(n - 1)
