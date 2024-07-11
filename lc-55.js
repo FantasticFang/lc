bb8f935e24f270727a24fd6eee5f31ff0d5e09c8
weijie9 <weijie9@staff.weibo.com>
2024-07-11 19:55:17 +0800

    update: lc-55

---
+++ lc-55.js
@@ -0,0 +1,41 @@
+/**
+55. Jump Game
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let canStep = 0 
+  let start = 0
+  const length = nums.length
+  for (let i = 0; i < length - 1; i++) { // 只需判定倒数第二步是否可走，如果可走就可完成
+    canStep = Math.max(canStep - 1, nums[i]) // 取当前位置还能走的最大距离
+    if (canStep > 0) { // 如果最大距离大于0就可以走了
+      start++
+    }
+  }
+  
+  return start === length - 1
+};
+
+canJump([2, 3, 1, 1, 4])
+
