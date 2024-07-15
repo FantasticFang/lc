5f1ff54afdc47434bcf2955cb7a384861ad714bc
weijie9 <weijie9@staff.weibo.com>
2024-07-15 18:19:08 +0800

    lc-45

--- lc-45.js
+++ lc-45.js
@@ -11,7 +11,7 @@ i + j < n
 [2,4,2,1,4,1,1]
 Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
 startIndex: 0, 1, 1, 1, 2, 2, 2
-previousValue: 2, 1, 0, 2 = nums[startIndx] - (i - stratIndex), 1, 0, 2
+previousValue: 2, 1, 0, 2 = nums[startIndex] - (i - startIndex), 1, 0, 2
 current : 0 + 2, 1 + 4, 2 + 2, 3 + 1, 4 + 4, 5 + 1, 6 + 1
 max: 2, 5, 5, 5, 8, 8, 8
 step: 1, 1, 1, 2, 2, 2, 3
@@ -28,15 +28,17 @@ var jump = function(nums) {
   let step = 0
   let max = nums[0]
   let startIndex = 0
-  for(let i = 1; i < nums.length; i++) {
-    max--
-    if(i < current) {
-      if(nums[i] + i > max) {
-        max = nums[i] + i
-        startIndex = i
-      }
-    } else {
+  for (let i = 1; i < nums.length; i++) {
+    previousValue-- // 走一步减一步
+    if (nums[i] + i > max) {
+      max = nums[i] + i // 最优值
+      startIndex = i
+    }
+    if (previousValue === 0 || i === nums.length - 1) { // 用完了步数，或者到达终点
       step++
+      previousValue = nums[startIndex] - (i - startIndex) // 当前index时，最优值能覆盖的范围
     }
   }
+
+  return step
 };
