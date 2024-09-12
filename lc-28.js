c9fd6e1c1e0539b2a71124e22a15bee140a41240
weijie9 <weijie9@staff.weibo.com>
2024-09-12 19:15:12 +0800

    kmp 暂存

--- lc-28.js
+++ lc-28.js
@@ -6,7 +6,6 @@
 Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
  
-
 Example 1:
 
 Input: haystack = "sadbutsad", needle = "sad"
@@ -31,9 +30,10 @@ haystack and needle consist of only lowercase English characters.
  * @param {string} needle
  * @return {number}
  */
-// 朴素解法
+// 朴素解法，BF算法，复杂度o(m * n)
 var strStr = function (haystack, needle) {
-  for (let start = 0; start <= haystack.length; start++) {
+  // for (let start = 0; start < haystack.length; start++) {
+  for (let start = 0; start <= haystack.length - needle.length; start++) { // 优化一下，当end - start的长度不足以支撑needle的长度时
     let end = start
     while (end - start < needle.length) {
       if (haystack[end] === needle[end - start]) {
@@ -51,6 +51,20 @@ var strStr = function (haystack, needle) {
 };
 
 // KMP算法
+function kmpNext(str) {
+  const next = [0] // 初始化，str的第一位的next为0
+  const length = str.length
+  for (let i = 1, j = next[0]; i < length; i++) {
+    while (j > 0 && str[j] !== str[i]) { 
+      j = next[j - 1] // 如何理解？
+    }
+    if (str[j] === str[i]) {
+      j++
+    }
+    next[i] = j
+  }
+}
+
 var strStr = function (haystack, needle) {
   
 };
