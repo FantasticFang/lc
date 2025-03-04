b46b3511c786db0856b66dca72cbd05083fcd60e
weijie9 <weijie9@staff.weibo.com>
2025-03-04 18:52:17 +0800

    lc-48

--- lc-48.js
+++ lc-48.js
@@ -27,3 +27,39 @@ n == matrix.length == matrix[i].length
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
  */
+
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  const n = matrix.length
+  if (n === 1) {
+    return
+  }
+  
+  function rotateOuter(matrix, currentRow, startCol, endCol) {
+    if (endCol - startCol < 1) {
+      return
+    }
+    
+    // 注意边界值，最后一个值已经在第一次参与旋转
+    for (let col = startCol; col < endCol - 1; col++) {
+      let i = currentRow, j = col // 旋转起始点
+      const rotateStart = matrix[i][j] // 旋转起始值
+      for (let k = 0; k < 3; k++) {
+        matrix[i][j] = matrix[n - 1 - j][i]
+        const tmp = i
+        i = n - 1 - j
+        j = tmp
+      }
+      matrix[i][j] = rotateStart
+    } 
+    
+    rotateOuter(matrix, currentRow + 1, startCol + 1, endCol - 1)
+  }
+  
+  rotateOuter(matrix, 0, 0, n)
+};
+
+rotate([[1,2,3],[4,5,6],[7,8,9]])
