b635246ff1389768580a5a1d1d63e522b4c1da09
weijie9 <weijie9@staff.weibo.com>
2025-08-22 16:08:00 +0800

    lc-202

---
+++ lc-202.js
@@ -0,0 +1,71 @@
+/**
+202. Happy Number
+简单
+相关标签
+premium lock icon
+相关企业
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+var isHappy = function (n) {
+  const map = {}
+  
+  function calculate(x) {
+    if (x === 1) {
+      return true
+    }
+    if (map[x]) {
+      return false
+    }
+    map[x] = true
+    
+    let res = 0
+    while (true) {
+      res += Math.pow(x % 10, 2)
+      if (x < 10) {
+        break
+      }
+      x = Math.floor(x / 10)
+    }
+    x = res
+
+    return calculate(x)
+  }
+  
+  return calculate(n)
+};
+
+console.log(isHappy(10))
